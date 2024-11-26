126dc68d5dc0d96339b3aea637bacb7e3f4253e1
Parag Khandelwal <paragtech97@gmail.com>
2024-11-26 16:36:00 -0500

    Updated Google OAuth configuration and Flask app port to 8080

--- app.py
+++ app.py
@@ -53,4 +53,4 @@ def logout():
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=10000)
+    app.run(debug=True, host='0.0.0.0', port=8080)
